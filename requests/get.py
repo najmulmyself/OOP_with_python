077e6f905ca171a7ec3b79fac302344f372f5cd6
Najmul <64252963+najmulmyself@users.noreply.github.com>
2021-07-31 02:03:29 +0600

    notes added

--- requests/get.py
+++ requests/get.py
@@ -1,5 +1,14 @@
 import requests
 
-r = requests.get('https://latestjobcircular.com')
+# http get requests
+
+r = requests.get('https://httpbin.org/get')
 
 print(r.status_code)
+
+# helpful resourse of requests tutorial is https://www.edureka.co/blog/python-requests-tutorial/
+# helpful resourse of requests tutorial is https://www.datacamp.com/community/tutorials/making-http-requests-in-python
+
+# few notes
+
+# you will have to pass stream=True in the request to get the raw response as per need
