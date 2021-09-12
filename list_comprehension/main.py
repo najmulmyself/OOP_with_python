c2aef7a46182f783adbea86e453a809a7485a295
Najmul Huda <najmul.myself@gmail.com>
2021-09-12 22:01:22 +0600

    exercise 3 completed

--- list_comprehension/main.py
+++ list_comprehension/main.py
@@ -1,8 +1,12 @@
-with open("file_1.txt") as file:
-    file1 = file.read()
-    list = [file for file in file1.split()]
+with open("file_1.txt") as file_1:
+    file1 = file_1.readlines()
 
-for n in list:
-    con = int(n)
-    if(con % 2 == 0):
-        print(con)
+
+with open("file_2.txt") as file_2:
+    file2 = file_2.readlines()
+
+
+# format = [new_item for item in list if test]
+
+result = [int(num) for num in file1 if num in file2]
+print (result)
