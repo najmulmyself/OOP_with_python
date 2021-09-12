5cad15831d41decdcd322796c05a696695a5e286
Najmul Huda <najmul.myself@gmail.com>
2021-09-12 21:27:00 +0600

    read and readlines

--- files/files.py
+++ files/files.py
@@ -16,3 +16,27 @@ with open("test.txt") as file:
     content = file.read()
     print(content) 
     # no need to close the file
+
+
+#IMPORTANT!
+#read() and readlines() has a diffrence | read just like read the file content and readlines() return a list of content 
+#for refs: https://www.w3schools.com/python/ref_file_readlines.asp
+
+
+#WRITING A FILE
+#WRITING A FILE NEDD TO SPECIFY THE MODE="W" | BY DEFAULT THE MODE IS MODE = "R";
+#no need to mention mode = "" we can just pass the mode value;
+
+with open("test.txt", "w") as file:
+    content = file.write("New Text")
+    print(content) 
+    
+    #if we use mode="w" it will replace the the existing text and write the new line
+    #if we want to add more lines in our files need to change the mode to mode="a" this simply means append the text to the existing file
+
+with open("test.txt", "a") as file:
+    content = file.write("\nNew Text with new line")
+    print(content) 
+
+
+#if we set the file name which doesn't even exits in our directory , write mode will create a new file for us | this only works in write mode
