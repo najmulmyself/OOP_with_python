6f0d864b76e591982cbd3cb063298383d4c51864
Najmul Huda <najmul.myself@gmail.com>
2021-09-12 09:56:35 +0600

    list comprehension formate

--- list_comprehension/list.py
+++ list_comprehension/list.py
@@ -9,3 +9,17 @@ for n in numbers:
 
 
 print (new_list)
+
+
+# list comprehension code will do the same
+
+numbers = [1 , 2 , 3 , 4, 5, 6]
+
+new_numbers = [n+1 for n in numbers]
+print (new_numbers)
+
+# list comprehension formate :
+
+# declare a list 
+
+# new_variables = [new_item for item in list]
