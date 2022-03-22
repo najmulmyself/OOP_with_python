e2f56203213715f1ca9c0b5b5363ec2e4294c998
najmulmyself <najmul.myself@gmail.com>
2022-03-23 02:41:52 +0600

    cant find title from the site

--- webScrapping/index.py
+++ webScrapping/index.py
@@ -1,1 +1,9 @@
-print('Hello World')
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'https://latestjobcircular.com/dhaka-thai-ltd-job-circular-november-2021/'
+r = requests.get(url)
+html = bs(r.text,'html.parser')
+eTitle = html.find('title')
+
+print(eTitle)
