336291fe52a427a9bc40f812e9cce22e8234709b
najmulmyself <najmul.myself@gmail.com>
2022-03-23 02:56:46 +0600

    managed to get the desired titie

--- webScrapping/index.py
+++ webScrapping/index.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://latestjobcircular.com/dhaka-thai-ltd-job-circular-november-2021/'
+url = 'https://www.indeed.com/jobs?q=python&l=Usk%2C%20WA&vjk=26dd1928feab499f'
 r = requests.get(url)
 html = bs(r.text,'html.parser')
-eTitle = html.find('title')
-
-print(eTitle)
+eTitle = html.find_all('div', {"class" : "job_seen_beacon"})
+for tag in eTitle:
+    title = tag.find_all('h2')
+    for loc in title:
+        print(loc.text)
