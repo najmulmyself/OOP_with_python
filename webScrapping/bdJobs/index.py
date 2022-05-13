d33a5c1579fec64d499ea05c82854dca30735e57
najmulmyself <najmul.myself@gmail.com>
2022-05-13 23:31:12 +0600

    need to parse body data

--- webScrapping/bdJobs/index.py
+++ webScrapping/bdJobs/index.py
@@ -1,2 +1,7 @@
-import request
+import requests
 from bs4 import BeautifulSoup as bs
+
+url = "https://jobs.bdjobs.com/jobsearch.asp?fcatId=4&icatId="
+r = requests.get(url)
+html = bs(r.text,'html.parser')
+print(html.text)
